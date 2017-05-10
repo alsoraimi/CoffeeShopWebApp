1a9c82245c04d5d7d1fda957a8cab012eca85a6d
alsoraimi <alsoraimi46@gmail.com>
2017-05-10 09:33:24 -0400

    initial commit

---
+++ lab20/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab20.Startup))]
+namespace lab20
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
