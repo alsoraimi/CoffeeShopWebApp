9a07253b154c13969cd37294933dfa80660488af
alsoraimi <alsoraimi46@gmail.com>
2017-05-16 10:01:41 -0400

    add option to add item, update item and delete item. added some validation

--- lab20/Controllers/HomeController.cs
+++ lab20/Controllers/HomeController.cs
@@ -12,13 +12,96 @@ namespace lab20.Controllers
     {
         public ActionResult Index()
         {
-            CoffeeShopDBEntities CoffeeShopDB = new CoffeeShopDBEntities();
+            CoffeeShopDBEntities CoffeeShopDB = new CoffeeShopDBEntities();   
             List<Item> ItemList = CoffeeShopDB.Items.ToList();
             ViewBag.ItemList = ItemList;
 
 
             return View();
         }
+
+        public ActionResult DeleteItem(string Name)
+
+        {
+
+            try
+            {
+
+                CoffeeShopDBEntities CoffeeShopDB = new CoffeeShopDBEntities();
+
+                if (Name == null)
+                {
+
+                    string ErrorMessage = "Error, Item Name does not exist";
+                    ViewBag.ErrorMessage = ErrorMessage;
+                    return View("ErrorMessages");
+                }
+
+                Item ToDelete = CoffeeShopDB.Items.Find(Name);
+                CoffeeShopDB.Items.Remove(ToDelete);
+                CoffeeShopDB.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            catch(Exception)
+            {
+                ViewBag.ErrorMessage = "Sorry! There seems to be an error.";
+                return View("ErrorMessages");
+
+
+            }
+
+            
+
+
+        }
+
+        public ActionResult UpdateItem(string Name)
+        {
+
+            CoffeeShopDBEntities CoffeeShopDB = new CoffeeShopDBEntities();
+            Item ToFind = CoffeeShopDB.Items.Find(Name);
+            return View("UpdateItem");
+            
+
+        }
+
+        public ActionResult SaveUpdates(Item ToBeUpdated)
+        {
+            CoffeeShopDBEntities CoffeeShopDB = new CoffeeShopDBEntities();
+            Item ToFind = CoffeeShopDB.Items.Find(ToBeUpdated.Name);
+            ToFind.Description = ToBeUpdated.Description;
+            ToFind.Price = ToBeUpdated.Price;
+            ToFind.Quantity = ToBeUpdated.Quantity;
+
+            CoffeeShopDB.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+
+        public ActionResult SaveNewItem(Item NewItem)
+        {
+
+            CoffeeShopDBEntities CoffeeShopDB = new CoffeeShopDBEntities();
+            CoffeeShopDB.Items.Add(NewItem);
+            CoffeeShopDB.SaveChanges();
+
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+
         public ActionResult Register()
         {
             return View();
---
+++ lab20/DAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab20
+{
+    public class DAO
+    {
+
+
+    }
+}
